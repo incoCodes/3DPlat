225166e79e69e9499b77724f904d4009e114abc9
incoCodes <supersonic220019@gmail.com>
2022-02-22 19:40:40 +0200

    Added jumping with grounded check

--- 3DPlat/Assets/Scripts/Player.cs
+++ 3DPlat/Assets/Scripts/Player.cs
@@ -21,10 +21,23 @@ public class Player : MonoBehaviour
             transform.forward = vel;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        // checks to see if we press the space bar as well as checking if we are grounded
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
+            // adds a upwards force that is multiplied by the jump force
+            isGrounded = false;
             rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Checks to see if the object that the player contacted with is facing upwards which is standing on the ground
+        if (collision.contacts[0].normal == Vector3.up)
+        {
+            // we call the player grounded so that he can only have one jump at a time
+            isGrounded = true;
+        }
     }
 }
